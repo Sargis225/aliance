9d2672fcd276322239352cfa1b3b4dcc14965f0d
Sargis <sargisske@mail.ru>
2021-01-14 18:18:53 +0400

    changes

--- SetupViewController.swift
+++ SetupViewController.swift
@@ -9,36 +9,41 @@ import UIKit
 
 class SetupViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return colorArray.count
+        return 5
     }
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
+        return 1
     }
+    var selectColor = ""
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let ident = "cellIdentifier"
         let cell = tableView.dequeueReusableCell(withIdentifier: ident) as! MyTableViewCell
-        if indexPath.section == 0 {
+        cell.textLabel?.textAlignment = .center
+        if indexPath.row == 0 {
+            cell.textLabel?.text = selectColor
+        }
         
-            cell.backgroundColor = colorArray[indexPath.row]
-            return cell
-        }
-        if indexPath.section == 1 {
-            cell.textLabel?.text = language[indexPath.row]
-            cell.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
-            return cell
-        }
-        if indexPath.section == 2 {
-            cell.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
-
-            if indexPath.row == 0 {
-                cell.textLabel?.text = "Ընտրեք խաղի ժամանակահատվածը"
-            }
-            if indexPath.row == 1 {
-                cell.textLabel?.text = "Ընտրեք առավելագույն միավորը"
-            }
-            
-            
+        
+//        if indexPath.section == 0 {
+//
+//            cell.backgroundColor = colorArray[indexPath.row]
+//            return cell
+//        }
+//        if indexPath.section == 1 {
+//            cell.textLabel?.text = language[indexPath.row]
+//            cell.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+//            return cell
+//        }
+//        if indexPath.section == 2 {
+//            cell.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+//
+//            if indexPath.row == 0 {
+//                cell.textLabel?.text = "Ընտրեք խաղի ժամանակահատվածը"
+//            }
+//            if indexPath.row == 1 {
+//                cell.textLabel?.text = "Ընտրեք առավելագույն միավորը"
+//            }
 //            if indexPath.row == 0 {
 //                let slider = UISlider(frame: CGRect(x: 10, y: 18, width: cell.frame.width - 80, height: 20))
 //                slider.maximumValue = 100
@@ -51,8 +56,8 @@ class SetupViewController: UIViewController, UITableViewDataSource, UITableViewD
 //                cell.addSubview(slider)
 //
 //            }
-            return cell
-        }
+//            return cell
+//        }
        return cell
     }
     
@@ -78,119 +83,126 @@ class SetupViewController: UIViewController, UITableViewDataSource, UITableViewD
     let language = ["Հայերեն","English","Русский"]
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.section == 0 {
-            let color = colorArray[indexPath.row]
-            allAnkapLable.backgroundColor = color
-            view.backgroundColor = color
-            UserDefaults.standard.setColor(color: color, forKey: "colorKey")
+        if indexPath.row == 0 {
+            performSegue(withIdentifier: "colorsIdent", sender: nil)
         }
         
-        if indexPath.section == 1{
-            UserDefaults.standard.set(language[indexPath.row],forKey: "languageKey")
-            switch indexPath.row {
-            case 0:
-                allAnkapLable.text = "Բոլոր Կարգավորումները"
-                selectColorLabel.text = "Ընտրեք Գույնը"
-                tableView.cellForRow(at: [2,0])?.textLabel?.text = "Ընտրեք խաղի ժամանակահատվածը"
-                tableView.cellForRow(at: [2,1])?.textLabel?.text = "Ընտրեք առավելագույն միավորը"
-
-                print(indexPath)
-            case 1:
-                allAnkapLable.text = "All settings"
-                selectColorLabel.text = "Select Color"
-                tableView.cellForRow(at: [2,0])?.textLabel?.text = "Select the game period"
-                tableView.cellForRow(at: [2,1])?.textLabel?.text = "Select the maximum score"
-            case 2:
-                allAnkapLable.text = "Общие настройки"
-                selectColorLabel.text = "Выбирайте цвет"
-                tableView.cellForRow(at: [2,0])?.textLabel?.text = "Выбирайте время игры"
-                tableView.cellForRow(at: [2,1])?.textLabel?.text = "Выбирайте максимальный очков"
-            default:
-                print("lav klni")
-            }
-        }
         
-        if indexPath.section == 2  && indexPath.row == 0 {
-            let langueage = UserDefaults.standard.string(forKey: "languageKey")
-            var massage = ""
-            var buttonCancelText = ""
-            var buttonRenameText = ""
-            switch langueage {
-            case "Հայերեն":
-                massage = "Ընտրեք խաղի ժամանակահատվածը"
-                buttonCancelText = "Չեղարկել"
-                buttonRenameText = "Փոխել"
-            case "English":
-               massage = "Select the game period"
-               buttonCancelText = "Cancel"
-               buttonRenameText = "Rename"
-            case "Русский":
-                massage = "Выбирайте время игры"
-                buttonCancelText = "Отменить"
-                buttonRenameText = "Изменить"
-            default:
-                print("ok")
-            }
-
-            let alertController: UIAlertController = UIAlertController(title: massage, message: "", preferredStyle: .alert)
-            let cancelAction: UIAlertAction = UIAlertAction(title: buttonCancelText, style: .cancel) { action -> Void in
-                    //cancel code
-                }
-            alertController.addAction(cancelAction)
-            
-            let nextAction: UIAlertAction = UIAlertAction(title: buttonRenameText, style: .default) { action -> Void in
-            let text = ((alertController.textFields?.first as! UITextField).text)!
-                UserDefaults.standard.set(text,forKey: "timeIntervalKey")
-                }
-            alertController.addAction(nextAction)
-                //Add text field
-            alertController.addTextField { (textField) -> Void in
-                    textField.textColor = UIColor.green
-                }
-                //Present the AlertController
-            present(alertController, animated: true, completion: nil)
-        }
         
-        if indexPath.section == 2  && indexPath.row == 1 {
-            let langueage = UserDefaults.standard.string(forKey: "languageKey")
-            var massage = ""
-            var buttonCancelText = ""
-            var buttonRenameText = ""
-            switch langueage {
-            case "Հայերեն":
-                massage = "Ընտրեք առավելագույն միավորը"
-                buttonCancelText = "Չեղարկել"
-                buttonRenameText = "Փոխել"
-            case "English":
-               massage = "Select the maximum score"
-               buttonCancelText = "Cancel"
-               buttonRenameText = "Rename"
-            case "Русский":
-                massage = "Выбирайте максимальный очков"
-                buttonCancelText = "Отменить"
-                buttonRenameText = "Изменить"
-            default:
-                print("ok")
-            }
-
-            let alertController: UIAlertController = UIAlertController(title: massage, message: "", preferredStyle: .alert)
-            let cancelAction: UIAlertAction = UIAlertAction(title: buttonCancelText, style: .cancel) { action -> Void in
-                    //cancel code
-                }
-            alertController.addAction(cancelAction)
-            
-            let nextAction: UIAlertAction = UIAlertAction(title: buttonRenameText, style: .default) { action -> Void in
-            let text = ((alertController.textFields?.first as! UITextField).text)!
-                UserDefaults.standard.set(text,forKey: "maximumPointsKey")
-                }
-            alertController.addAction(nextAction)
-                //Add text field
-            alertController.addTextField { (textField) -> Void in
-                    textField.textColor = UIColor.green
-                }
-                //Present the AlertController
-            present(alertController, animated: true, completion: nil)
-        }
+        
+//        if indexPath.section == 0 {
+////            let color = colorArray[indexPath.row]
+////            allAnkapLable.backgroundColor = color
+////            view.backgroundColor = color
+//            UserDefaults.standard.setColor(color: color, forKey: "colorKey")
+//        }
+        
+//        if indexPath.section == 1{
+//            UserDefaults.standard.set(language[indexPath.row],forKey: "languageKey")
+//            switch indexPath.row {
+//            case 0:
+//                allAnkapLable.text = "Բոլոր Կարգավորումները"
+//                selectColorLabel.text = "Ընտրեք Գույնը"
+//                tableView.cellForRow(at: [2,0])?.textLabel?.text = "Ընտրեք խաղի ժամանակահատվածը"
+//                tableView.cellForRow(at: [2,1])?.textLabel?.text = "Ընտրեք առավելագույն միավորը"
+//
+//                print(indexPath)
+//            case 1:
+//                allAnkapLable.text = "All settings"
+//                selectColorLabel.text = "Select Color"
+//                tableView.cellForRow(at: [2,0])?.textLabel?.text = "Select the game period"
+//                tableView.cellForRow(at: [2,1])?.textLabel?.text = "Select the maximum score"
+//            case 2:
+//                allAnkapLable.text = "Общие настройки"
+//                selectColorLabel.text = "Выбирайте цвет"
+//                tableView.cellForRow(at: [2,0])?.textLabel?.text = "Выбирайте время игры"
+//                tableView.cellForRow(at: [2,1])?.textLabel?.text = "Выбирайте максимальный очков"
+//            default:
+//                print("lav klni")
+//            }
+//        }
+        
+//        if indexPath.section == 2  && indexPath.row == 0 {
+//            let langueage = UserDefaults.standard.string(forKey: "languageKey")
+//            var massage = ""
+//            var buttonCancelText = ""
+//            var buttonRenameText = ""
+//            switch langueage {
+//            case "Հայերեն":
+//                massage = "Ընտրեք խաղի ժամանակահատվածը"
+//                buttonCancelText = "Չեղարկել"
+//                buttonRenameText = "Փոխել"
+//            case "English":
+//               massage = "Select the game period"
+//               buttonCancelText = "Cancel"
+//               buttonRenameText = "Rename"
+//            case "Русский":
+//                massage = "Выбирайте время игры"
+//                buttonCancelText = "Отменить"
+//                buttonRenameText = "Изменить"
+//            default:
+//                print("ok")
+//            }
+//
+//            let alertController: UIAlertController = UIAlertController(title: massage, message: "", preferredStyle: .alert)
+//            let cancelAction: UIAlertAction = UIAlertAction(title: buttonCancelText, style: .cancel) { action -> Void in
+//                    //cancel code
+//                }
+//            alertController.addAction(cancelAction)
+//
+//            let nextAction: UIAlertAction = UIAlertAction(title: buttonRenameText, style: .default) { action -> Void in
+//            let text = ((alertController.textFields?.first as! UITextField).text)!
+//                UserDefaults.standard.set(text,forKey: "timeIntervalKey")
+//                }
+//            alertController.addAction(nextAction)
+//                //Add text field
+//            alertController.addTextField { (textField) -> Void in
+//                    textField.textColor = UIColor.green
+//                }
+//                //Present the AlertController
+//            present(alertController, animated: true, completion: nil)
+//        }
+//
+//        if indexPath.section == 2  && indexPath.row == 1 {
+//            let langueage = UserDefaults.standard.string(forKey: "languageKey")
+//            var massage = ""
+//            var buttonCancelText = ""
+//            var buttonRenameText = ""
+//            switch langueage {
+//            case "Հայերեն":
+//                massage = "Ընտրեք առավելագույն միավորը"
+//                buttonCancelText = "Չեղարկել"
+//                buttonRenameText = "Փոխել"
+//            case "English":
+//               massage = "Select the maximum score"
+//               buttonCancelText = "Cancel"
+//               buttonRenameText = "Rename"
+//            case "Русский":
+//                massage = "Выбирайте максимальный очков"
+//                buttonCancelText = "Отменить"
+//                buttonRenameText = "Изменить"
+//            default:
+//                print("ok")
+//            }
+//
+//            let alertController: UIAlertController = UIAlertController(title: massage, message: "", preferredStyle: .alert)
+//            let cancelAction: UIAlertAction = UIAlertAction(title: buttonCancelText, style: .cancel) { action -> Void in
+//                    //cancel code
+//                }
+//            alertController.addAction(cancelAction)
+//
+//            let nextAction: UIAlertAction = UIAlertAction(title: buttonRenameText, style: .default) { action -> Void in
+//            let text = ((alertController.textFields?.first as! UITextField).text)!
+//                UserDefaults.standard.set(text,forKey: "maximumPointsKey")
+//                }
+//            alertController.addAction(nextAction)
+//                //Add text field
+//            alertController.addTextField { (textField) -> Void in
+//                    textField.textColor = UIColor.green
+//                }
+//                //Present the AlertController
+//            present(alertController, animated: true, completion: nil)
+//        }
         
 //        print(indexPath.row)
     }
@@ -200,7 +212,7 @@ class SetupViewController: UIViewController, UITableViewDataSource, UITableViewD
     
 
     
-    @IBOutlet var selectColorLabel: UILabel!
+    
     @IBOutlet var allAnkapLable: UILabel!
     
     @IBOutlet var setupTableView: UITableView!
